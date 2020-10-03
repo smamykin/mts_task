2bd7d9b097cee19e10839ec99184fcbeb80caddf
Mamykin Sergei <mamykin.sergei@gmail.com>
2020-10-03 19:33:01 +0300

    3 | 0h 58m | чтобы на всех этапах иметь уверенность что тип действия задан верным строковым литералом

---
+++ src/ValueObject/ActionType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\ValueObject;
+
+use OutOfBoundsException;
+
+final class ActionType
+{
+    const IN = 'in';
+    const OUT = 'out';
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @param string $type
+     */
+    public function __construct(string $type)
+    {
+        if (!in_array($type, [self::IN, self::OUT])) {
+            throw new OutOfBoundsException('Доступны только два типа действий: in(заезд), out(выезд).');
+        }
+        $this->type = $type;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getType();
+    }
+
+    private function getType(): string
+    {
+        return $this->type;
+    }
+}
